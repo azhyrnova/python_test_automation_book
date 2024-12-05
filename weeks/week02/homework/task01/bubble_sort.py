e488108c9d1ee9ad168ea9ecf077c10720cfd7c4
Anastasiia Zhyrnova <anastasiia_zhyrnova@epam.com>
2024-12-05 02:02:44 +0100

    applied fixed after review

--- weeks/week02/homework/task01/bubble_sort.py
+++ weeks/week02/homework/task01/bubble_sort.py
@@ -1,4 +1,6 @@
-#Ascending Order: Sorts by always moving larger elements to the end
+"""
+Ascending Order: Sorts by always moving larger elements to the end
+"""
 test_list = [10, 1, 2, 3, 8, 5, 6, 7, 11, -2]
 
 def swap (arr, pos1, pos2):
@@ -6,37 +8,47 @@ def swap (arr, pos1, pos2):
     
 
 def bubble_sort_ascending(arr):
-    for itm in arr:
-        for idx in range(len(arr) - 1):
-            if arr[idx] > arr [idx +1]:
-                swap (arr, idx, idx + 1)
+    n = len(arr)
+    for i in range(n - 1): #outer loop
+        for j in range(n - 1 - i): #inner loop for comparing numbers next to each other
+            if arr[j] > arr[j + 1]:
+                swap(arr, j, j + 1)
     return arr 
 
 print("Sorted list in ASC order:" , bubble_sort_ascending(test_list))
 
-#Descending Order: Sorts by always moving smaller elements to the end  
+"""
+Descending Order: Sorts by always moving smaller elements to the end  
+"""
 
 def bubble_sort_descending(arr):
     n = len(arr)
-    for idx in range(n - 1):
-            if arr[idx] < arr [idx +1]:
-                swap (arr, idx, idx + 1)
+    for i in range(n - 1): #outer loop
+        for j in range(n - 1 - i): #inner loop for comparing numbers next to each other
+            if arr[j] < arr[j + 1]:
+                swap(arr, j, j + 1)
     return arr 
 
 print("Sorted list in DESC order:" , bubble_sort_descending(test_list))
 
-#Early Stopping: Improves efficiency by halting if no swaps are made during a pass, meaning the list is already sorted
+"""
+Early Stopping: Improves efficiency by halting if no swaps are made during a pass, meaning the list is already sorted
+"""
 
 def bubble_sort_descending_with_early_stopping(arr):
     n = len(arr)
-    for i in range(n - 1):
+    for i in range(n - 1): #outer loop
         swapped = False
-        for idx in range(n - 1 - i):
-            if arr[idx] < arr [idx +1]:
-                swap (arr, idx, idx + 1)
+        for j in range(n - 1 - i): #inner loop for comparing numbers next to each other
+            if arr[j] < arr[j + 1]:
+                swap(arr, j, j + 1)
                 swapped = True
-        if not swapped: 
-            break
+            if not swapped: 
+                break
     return arr 
 
-print("Sorted list in DESC order with early stop:" , bubble_sort_descending_with_early_stopping(test_list))    
+print("Sorted list in DESC order with early stop:" , bubble_sort_descending_with_early_stopping(test_list))
+
+test_list = [10, 1, 2, -3, 8, -5, 6, 7, 11, -2]
+
+print(bubble_sort_descending(test_list))   
